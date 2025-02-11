37654e938bd70d8c347bfb13153e1649ba6bf227
Jacob_Cozens <jacobcozens@rocketmail.com>
2025-02-11 12:03:12 -0500

    add Section class

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 import { initialTodos, validationConfig } from "../utils/constants.js";
 import ImportedTodo from "../components/Todo.js";
 import ImportedFormValidator from "../components/FormValidator.js";
+import ImportedSection from "../components/Section.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector("#add-todo-popup");
@@ -11,18 +12,45 @@ const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
 // const todoTemplate = document.querySelector("#todo-template");
 const todosList = document.querySelector(".todos__list");
 
+function handleCheck(completed) {
+  todoCounter.updateCompleted(completed);
+}
+
+function handleDelete(completed) {
+  if (completed) {
+    todoCounter.updateCompleted(false);
+  }
+  todoCounter.updateTotal(false);
+}
+
+const generateTodo = (data) => {
+  const todo = new ImportedTodo(
+    data,
+    "#todo-template",
+    handleCheck,
+    handleDelete
+  );
+  const todoElement = todo.getView();
+  return todoElement;
+};
+
+const section = new ImportedSection({
+  items: initialTodos,
+  renderer: (item) => {
+    const todo = generateTodo(item);
+    section.addItem(todo);
+  },
+  containerSelector: ".todos__list",
+});
+
+section.renderItems();
+
 const openModal = (modal) => {
   modal.classList.add("popup_visible");
 };
 
 const closeModal = (modal) => {
   modal.classList.remove("popup_visible");
-};
-
-const generateTodo = (data) => {
-  const Todo = new ImportedTodo(data, "#todo-template");
-  const todoElement = Todo.getView();
-  return todoElement;
 };
 
 addTodoButton.addEventListener("click", () => {
@@ -49,10 +77,8 @@ addTodoForm.addEventListener("submit", (evt) => {
   closeModal(addTodoPopup);
 });
 
-initialTodos.forEach((item) => {
-  const todo = generateTodo(item);
-  todosList.append(todo);
-});
-
-const newFormValidator =   new ImportedFormValidator(validationConfig, addTodoForm);
+const newFormValidator = new ImportedFormValidator(
+  validationConfig,
+  addTodoForm
+);
 newFormValidator.enableValidation();
